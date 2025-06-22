5c30bc3246893be4c27175c5d5b412be1cda6669
Puneet <puneet19338@gmail.com>
2025-06-22 21:56:33 +0530

    Fix

--- socket.js
+++ socket.js
@@ -18,7 +18,7 @@ const getUser = (userId) => {
 export const initSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: process.env.CLIENT_URL,
       credentials: true,
     },
   });
